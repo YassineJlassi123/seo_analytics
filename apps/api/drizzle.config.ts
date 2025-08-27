996e5992a122bc235cd9c30b2e6e6e458bf1fc66
unknown <yassinejj696@gmail.com>
2025-08-27 03:12:33 +0200

    fixing the package.json for the backend

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,3 +1,13 @@
-import config from './drizzle.config.cjs';
+require('dotenv').config();
 
-export default config;
+module.exports = {
+  schema: './src/db/schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+  schemaFilter: ["public"],
+  verbose: true,
+  strict: true,
+};
